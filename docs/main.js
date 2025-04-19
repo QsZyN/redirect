6dfc3fe969c4c84a3766c23c33e23e57db6caeae
QsZyN <149283132+QsZyN@users.noreply.github.com>
2025-04-19 13:37:00 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -3,5 +3,5 @@ let params = url.searchParams;
 if (params.get('to') ===  null){
     window.location.href="https://github.com/QsZyN/redirect";
 }else{
-    window.location.href=decodeURIComponent(params.get('to'));
+    window.location.replace(decodeURIComponent(params.get('to')));
 }
